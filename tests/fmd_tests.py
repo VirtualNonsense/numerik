ef21841c24cd53620878aed3ceb6a4957d9b359e
Andreas Nachtmann <virtualnonsense@mailbox.org>
2021-11-03 08:06:27 +0100

    introducing new testcase

--- tests/fmd_tests.py
+++ tests/fmd_tests.py
@@ -7,6 +7,27 @@ def u(x: Union[float, ArrayLike]) -> Union[float, ArrayLike]:
     return np.exp(-x) + np.exp(x) + 1
 
 
+class FDMSolverTests(unittest.TestCase):
+    def __init__(self, n: int = 5):
+        super().__init__()
+        self.n = n
+
+    def Ex_2_1_a(self):
+        def k(x: float):
+            return 1
+
+        def f(x: float):
+            return np.exp(x) - np.exp(-x) + 1
+
+        interval = [0, 1]
+        h = (interval[1] - interval[0]) / self.n
+        x = np.arange(start=interval[0], stop=interval[1], step=h)
+
+        solution = u(x)
+        fdm_solver(k, k, k, h, interval)
+        self.assertTrue(True, False)
+
+
 class AMatrixTests(unittest.TestCase):
     def script_example(self):
         n = 16
