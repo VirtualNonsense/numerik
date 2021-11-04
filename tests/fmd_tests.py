d5ab6ac7cf1b68ca2579eecc1cc9c828f258ebea
Andreas Nachtmann <virtualnonsense@mailbox.org>
2021-11-04 12:00:57 +0100

    fixed unit tests

--- tests/fmd_tests.py
+++ tests/fmd_tests.py
@@ -12,7 +12,7 @@ class FDMSolverTests(unittest.TestCase):
         super().__init__()
         self.n = n
 
-    def Ex_2_1_a(self):
+    def test_Ex_2_1_a(self):
         def k(x: float):
             return 1
 
@@ -29,7 +29,7 @@ class FDMSolverTests(unittest.TestCase):
 
 
 class AMatrixTests(unittest.TestCase):
-    def script_example_test(self):
+    def test_script_example(self):
         n = 16
 
         def k(x: float):
@@ -50,23 +50,16 @@ class AMatrixTests(unittest.TestCase):
         # generate A
         r = q
         N = x.shape[0] - 1
-        A, _, _, _ = gen_A_matrix(x, k, r, q, N, h)
-        A_ref = [[512, -256, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.],
-                 [-256, 512, -256, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.],
-                 [0, -256, 512, -256, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.],
-                 [0, 0, -256, 512, -256, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.],
-                 [0, 0, 0, -256, 512, -256, 0, 0, 0, 0, 0, 0, 0, 0, 0.],
-                 [0, 0, 0, 0, -256, 512, -256, 0, 0, 0, 0, 0, 0, 0, 0.],
-                 [0, 0, 0, 0, 0, -256, 512, -256, 0, 0, 0, 0, 0, 0, 0.],
-                 [0, 0, 0, 0, 0, 0, -256, 512, -256, 0, 0, 0, 0, 0, 0.],
-                 [0, 0, 0, 0, 0, 0, 0, -256, 512, -256, 0, 0, 0, 0, 0.],
-                 [0, 0, 0, 0, 0, 0, 0, 0, -256, 512, -256, 0, 0, 0, 0.],
-                 [0, 0, 0, 0, 0, 0, 0, 0, 0, -256, 512, -256, 0, 0, 0.],
-                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -256, 512, -256, 0, 0.],
-                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -256, 512, -256, 0.],
-                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -256, 512, -256.],
-                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -256, 512.]]
-        self.assertTrue((A_ref == A).all())
+        a, b, c = gen_A_matrix(x, k, r, q, N, h)
+        a_ref = np.zeros(shape=15)
+        a_ref[1:] = -256
+        b_ref = np.ones(shape=16)
+        b_ref[1:-1] = 512
+        c_ref = np.zeros(shape=15)
+        c_ref[:-1] = -256
+        self.assertTrue((a == a_ref).all())
+        self.assertTrue((b == b_ref).all())
+        self.assertTrue((c == c_ref).all())
 
 
 if __name__ == '__main__':
