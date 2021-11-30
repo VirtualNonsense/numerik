3cd8be30e81eeb4fe9ab0874c726e36dc6ba7979
Andreas Nachtmann <virtualnonsense@mailbox.org>
2021-11-30 13:11:57 +0100

    first test is green

--- tests/fmb_ivbc_tests.py
+++ tests/fmb_ivbc_tests.py
@@ -5,18 +5,30 @@ from src.fdm_ivbc_solver import *
 
 # noinspection DuplicatedCode
 class FDMIVBCSolverTests(unittest.TestCase):
-    m_p_1 = 20
-    n_p_1 = 20
+    m_explicit = 5
+    n_explicit = 2
+    m_cn = 80
+    n_cn = 7
+    m_implicit = 80
+    n_implicit = 7
     t_start = 0
     t_end = 1
 
     a = 0
     b = 1
 
-    t = (t_start, t_end, m_p_1)
-    x = (a, b, n_p_1)
+    t_explicit = (t_start, t_end, m_explicit)
+    x_explicit = (a, b, n_explicit)
+    t_cn = (t_start, t_end, m_cn)
+    x_cn = (a, b, n_cn)
+    t_implicit = (t_start, t_end, m_implicit)
+    x_implicit = (a, b, n_implicit)
 
     sigma = 0
+
+    epsilon_explicit = 5e-2
+    epsilon_crank_nicolson = 5e-2
+    epsilon_implicit = 5e-2
 
     def test_Ex_2_2_a(self):
         u = lambda x, t: np.sin(x) * np.cos(t)
@@ -30,8 +42,8 @@ class FDMIVBCSolverTests(unittest.TestCase):
 
         f = lambda x, t: np.sin(x) * (2 * np.cos(t) - np.sin(t))
 
-        explicit = fdm_ivbc_solver(space=self.x,
-                                   time=self.t,
+        explicit = fdm_ivbc_solver(space=self.x_explicit,
+                                   time=self.t_explicit,
                                    k=k,
                                    q=q,
                                    f=f,
@@ -40,8 +52,8 @@ class FDMIVBCSolverTests(unittest.TestCase):
                                    phi=phi,
                                    sigma=0)
 
-        crank_nicolson = fdm_ivbc_solver(space=self.x,
-                                         time=self.t,
+        crank_nicolson = fdm_ivbc_solver(space=self.x_cn,
+                                         time=self.t_cn,
                                          k=k,
                                          q=q,
                                          f=f,
@@ -50,8 +62,8 @@ class FDMIVBCSolverTests(unittest.TestCase):
                                          phi=phi,
                                          sigma=1 / 2)
 
-        implicit = fdm_ivbc_solver(space=self.x,
-                                   time=self.t,
+        implicit = fdm_ivbc_solver(space=self.x_implicit,
+                                   time=self.t_implicit,
                                    k=k,
                                    q=q,
                                    f=f,
@@ -59,7 +71,19 @@ class FDMIVBCSolverTests(unittest.TestCase):
                                    mu_b=mu_b,
                                    phi=phi,
                                    sigma=1)
-        self.assertTrue(False)
+
+        tmp = explicit
+        xx, tt = np.meshgrid(tmp[1], tmp[2])
+        diff_explicit = u(xx, tt) - explicit[0]
+        tmp = crank_nicolson
+        xx, tt = np.meshgrid(tmp[1], tmp[2])
+        diff_crank_nicolson = u(xx, tt) - crank_nicolson[0]
+        tmp = implicit
+        xx, tt = np.meshgrid(tmp[1], tmp[2])
+        diff_implicit = u(xx, tt) - implicit[0]
+        self.assertTrue((np.abs(diff_explicit) < self.epsilon_explicit).all())
+        self.assertTrue((np.abs(diff_crank_nicolson) < self.epsilon_crank_nicolson).all())
+        self.assertTrue((np.abs(diff_implicit) < self.epsilon_implicit).all())
 
     def test_Ex_2_2_b(self):
         u = lambda x, t: np.exp(-2 * t) * np.cos(np.pi * x)
